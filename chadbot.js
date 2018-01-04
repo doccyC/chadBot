22cee47ce979fc92fb52694020be3b1811bae177
doccyC <35085401+doccyC@users.noreply.github.com>
2018-01-04 02:12:44 -0500

    Update chadbot.js

--- chadbot.js
+++ chadbot.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log("Let's go chowder some native chicks down that trail of beers!");
 });
+
+client.User.setGame('the game, bro');
 
 client.on('message', message => {
     if (message.content === '>>suhbrah') {

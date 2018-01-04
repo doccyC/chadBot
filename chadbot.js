a2e0972fe0448191c7e66922e47e70920ee52e00
doccyC <35085401+doccyC@users.noreply.github.com>
2018-01-04 18:06:36 -0500

    Update chadbot.js

--- chadbot.js
+++ chadbot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const PREFIX ">>";
 
 client.on('ready', () => {
     console.log("Let's go chowder some native chicks down that trail of beers!");
@@ -9,9 +10,11 @@ bot.user.setStatus('the game, bro');
 
 client.on('message', message => {
     if (message.content === '>>suhbrah') {
-    	message.reply('```Suh```');
+    	message.reply('`Suh`');
   	}
-});
+    if (message.content.startsWith(PREFIX)) return;
+    var args = message.content.substring(PREFIX.length).split(" ");
+})
 
 // no change, bro
 client.login(process.env.BOT_TOKEN);
